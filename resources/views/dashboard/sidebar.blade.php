1816697654c0b26f81d6779aec125657fc08794b
brunosmagna <bruno@magnaestrategia.com>
2016-09-19 00:01:25 -0300

    Ajuste no sidebar do novo tema

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -21,83 +21,97 @@
         <br>
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="clearfix"></div>
+            
             <div class="menu_section">
-                <h3>General</h3>
+                <h3>Dashboads</h3>
                 <ul class="nav side-menu">
-                    <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    <li><a><i class="fa fa-home"></i> Dashboard <span class="fa fa-chevron-down"></span></a>
                         <ul class="nav child_menu">
-                            <li><a href="index.html">Dashboard</a></li>
-                            <li><a href="index2.html">Dashboard2</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="form.html">General Form</a></li>
-                            <li><a href="form_advanced.html">Advanced Components</a></li>
-                            <li><a href="form_validation.html">Form Validation</a></li>
-                            <li><a href="form_wizards.html">Form Wizard</a></li>
-                            <li><a href="form_upload.html">Form Upload</a></li>
-                            <li><a href="form_buttons.html">Form Buttons</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="general_elements.html">General Elements</a></li>
-                            <li><a href="media_gallery.html">Media Gallery</a></li>
-                            <li><a href="typography.html">Typography</a></li>
-                            <li><a href="icons.html">Icons</a></li>
-                            <li><a href="glyphicons.html">Glyphicons</a></li>
-                            <li><a href="widgets.html">Widgets</a></li>
-                            <li><a href="invoice.html">Invoice</a></li>
-                            <li><a href="inbox.html">Inbox</a></li>
-                            <li><a href="calendar.html">Calendar</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="tables.html">Tables</a></li>
-                            <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="chartjs.html">Chart JS</a></li>
-                            <li><a href="chartjs2.html">Chart JS2</a></li>
-                            <li><a href="morisjs.html">Moris JS</a></li>
-                            <li><a href="echarts.html">ECharts </a></li>
-                            <li><a href="other_charts.html">Other Charts </a></li>
+                            <li><a href="/productsdashboard">Dashboard de Productos</a></li>
                         </ul>
                     </li>
                 </ul>
             </div>
             <div class="menu_section">
-                <h3>Live On</h3>
+                <h3>Produtos</h3>
+                
                 <ul class="nav side-menu">
-                    <li><a><i class="fa fa-bug"></i> Additional Pages <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="e_commerce.html">E-commerce</a></li>
-                            <li><a href="e_commerce_backend.html">E-commerce Backend</a></li>
-                            <li><a href="projects.html">Projects</a></li>
-                            <li><a href="project_detail.html">Project Detail</a></li>
-                            <li><a href="contacts.html">Contacts</a></li>
-                            <li><a href="profile.html">Profile</a></li>
-                            <li><a href="real_estate.html">Real Estate</a></li>
+                    <li><a><i class="fa fa-edit"></i> Linhas <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/lines">Ver Todas</a></li>
+                        <li><a href="/lines/create">Nova Linha</a></li>
+                    </ul>
+                </ul>
+                
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Referências <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/references">Ver Todas</a></li>
+                        <li><a href="/references/create">Nova Referência</a></li>
+                    </ul>
+                </ul>
 
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-windows"></i> Extras <span class="fa fa-chevron-down"></span></a>
-                        <ul class="nav child_menu">
-                            <li><a href="page_404.html">404 Error</a></li>
-                            <li><a href="page_500.html">500 Error</a></li>
-                            <li><a href="coming_soon.html">Coming Soon</a></li>
-                            <li><a href="plain_page.html">Plain Page</a></li>
-                            <li><a href="login.html">Login Page</a></li>
-                            <li><a href="sign_up.html">SignUp Page</a></li>
-                            <li><a href="pricing_tables.html">Pricing Tables</a></li>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Materiais <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/materials">Ver Todos</a></li>
+                        <li><a href="/materials/create">Novo Material</a></li>
+                    </ul>
+                </ul>
 
-                        </ul>
-                    </li>
-                    <li><a><i class="fa fa-laptop"></i> Landing Page <span class="label label-success pull-right">Coming Soon</span></a></li>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Cores <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/colors">Ver Todas</a></li>
+                        <li><a href="/colors/create">Nova Cor</a></li>
+                    </ul>
+                </ul>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Produtos <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/producto">Ver Todos</a></li>
+                        <li><a href="/producto/create">Nova Produto</a></li>
+                    </ul>
+                </ul>
+            </div>
+            <div class="menu_section">
+                <h3>Regiones</h3>
+                
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Macro Regiones <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/macroregions">Ver Todas</a></li>
+                        <li><a href="/macroregions/create">Crear</a></li>
+                    </ul>
+                </ul>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Macro Regiones <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/regions">Ver Todas</a></li>
+                        <li><a href="/regions/create">Crear</a></li>
+                    </ul>
+                </ul>
+            </div>
+
+
+            <div class="menu_section">
+                <h3>Sistema</h3>
+                
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Usuários<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/profile">Perfil</a></li>
+                        <li><a href="/users/create">Criar Novo</a></li>
+                         <li><a href="/users/1/edit">Editar Informações</a></li>
+                    </ul>
+                </ul>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-edit"></i> Ayuda <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                        <li><a href="/help">Productos</a></li>
+                        <li><a href="/help">Reprentativos</a></li>
+                        <li><a href="/help">Mi Perfil</a></li>
+                    </ul>
                 </ul>
             </div>
 
