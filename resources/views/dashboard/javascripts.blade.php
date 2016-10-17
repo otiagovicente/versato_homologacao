a2f0cd5cb414c505c9325e2ddb9b862bd3e14ce2
brunosmagna <bruno@magnaestrategia.com>
2016-10-16 22:15:31 -0200

    retornando pedidos

--- resources/views/dashboard/javascripts.blade.php
+++ resources/views/dashboard/javascripts.blade.php
@@ -36,13 +36,4 @@
         <script src="/dashboard/custom/notifications.js" type="text/javascript" charset="utf-8"></script>
     
         <!-- END THEME LAYOUT SCRIPTS -->
-        <script src="/dashboard/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
-        <script src="/dashboard/global/plugins/bootstrap-wizard/jquery.bootstrap.wizard.min.js" type="text/javascript"></script>
-        <script src="/dashboard/pages/scripts/form-wizard.js" type="text/javascript"></script>
-        
         @yield('scripts')
