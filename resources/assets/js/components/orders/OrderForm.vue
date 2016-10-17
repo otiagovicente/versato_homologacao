4de53b524ebedc2b588ee2f5c6765fad4a4b1523
brunosmagna <bruno@magnaestrategia.com>
2016-10-17 20:30:42 -0200

    pedidos versão form

--- resources/assets/js/components/orders/OrderForm.vue
+++ resources/assets/js/components/orders/OrderForm.vue
@@ -6,7 +6,201 @@
 </style>
 
 <template>
-    bla bla
+    <div class="content">
+        <div class="portlet light">
+            <div class="portlet-title">
+                <div class="caption font-blue">
+                    <i class="fa fa-plus font-blue"></i>Crear Pedido
+                </div>
+            </div>
+            <div class="portlet-body form">
+                
+                <div class="row">
+                    <div class="col-md-4">
+                        <small>Cliente</small>
+                        <div class="form-group form-line-input" id="client">
+                            <v-select 
+                                v-bind:options.sync="customers_select" :value.sync="order.customer_id"
+                                placeholder="Elije el cliente" class="form-control"
+                                id="customer-input" name="customer[]"
+                                search justified required close-on-select>
+                            </v-select>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <small>Representante</small>
+                        <div class="form-group form-line-input" id="representative">
+                            <v-select 
+                                v-bind:options.sync="representatives_select" :value.sync="order.representative_id"
+                                placeholder="Elije el representante" class="form-control"
+                                id="representative-input" name="representative[]"
+                                search justified required close-on-select>
+                            </v-select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <small>Cliente Descuento</small>
+                        <div class="form-group form-line-input" id="client_discount">
+                            <input 
+                                id="client-discount-input" 
+                                class="form-control input-sm" 
+                                type="number"
+                                v-model="order.client_discount" 
+                            />
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <small>Representante Descuento</small>
+                        <div class="form-group form-line-input" id="representative_discount">
+                            <input              
+                                id="representative-discount-input" 
+                                class="form-control input-sm" 
+                                type="number"
+                                v-model="order.representative_discount" 
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <fieldset class="form-group">
+                        <legend>Productos</legend>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="pull-right">
+                                    <a class="btn blue btn-outline sbold" 
+                                        data-toggle="modal" 
+                                        href="#large"
+                                    > 
+                                        Adicionar Producto 
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <table class="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Code</th>
+                                    <th>Costo</th>
+                                    <th>Precio</th>
+                                    <th>Descuento Cliente</th>
+                                    <th>Descuento Representante</th>
+                                    <th>Total</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="op in order.orderProducts">
+                                    <td>{{op.code}}</td>
+                                    <td>{{op.cost}}</td>
+                                    <td>{{op.price}}</td>
+                                    <td>{{op.client_discount}}</td>
+                                    <td>{{op.representative_discount}}</td>
+                                    <td>{{op.total}}</td>
+                                    <td>
+                                        <button 
+                                            class="btn grey"
+                                            @click="deleteProduct($index)"
+                                        >
+                                            Excluir
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </fieldset>
+                </div>
+                <div class="row">
+                    <div class="container-fluid">
+                        <div class="col-md-3 pull-right">
+                            <div class="form-group">
+                                <button type="button" @click="submitData" class="btn blue btn-block" id="send-btn">Guardar</button>
+                            </div>
+                        </div>
+                        <div class="col-md-3 pull-right">
+                            <div class="form-group">
+                                <a href="/orders/"><button type="button" class="btn grey btn-block" id="cancel-btn">Retornar</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Begin - Modal de Produtos -->
+    <div class="modal fade bs-modal-lg" id="large" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title">Productos</h4>
+                </div>
+                
+                <div class="modal-body">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Producto</>
+                                <th>Costo</th>
+                                <th>Precio</th>
+                                <th>Descuento Cliente</th>
+                                <th>Descuento Representante</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="p in products">
+                                <td>
+                                    <img class="product-list-photo" :src="p.photo" :alt="p.code" />
+                                    <br/>
+                                    <small>{{p.code}}</small>
+                                </td>
+                                <td>{{p.cost}}</td>
+                                <td>{{p.price}}</td>
+                                <td>
+                                    <input 
+                                        id="code-input" 
+                                        class="form-control input-sm" 
+                                        type="number"
+                                        v-model="p.client_discount" 
+                                    />
+                                </td>
+                                <td>
+                                    <input 
+                                        id="code-input" 
+                                        class="form-control input-sm" 
+                                        type="number"
+                                        v-model="p.representative_discount" 
+                                    />
+                                </td>
+                                <td>
+                                    <a
+                                        class="btn blue btn-outline sbold" 
+                                        data-toggle="modal" 
+                                        href="#large"
+                                        @click="addToProductList(p)"
+                                    > 
+                                        Selecionar
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                            </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn dark btn-outline" data-dismiss="modal">Fechar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--End - Modal de Produtos -->
+    <pre>
+        {{order | json}} 
+    </pre>
 </template>
 
 <script>
@@ -151,10 +345,6 @@ export default{
         },
         
         insertData: function(){
-            Vue.http.interceptors.push((request, next) => {
-                request.headers['X-CSRF-TOKEN'] = Laravel.csrfToken;
-                next();
-            });
             this.$http.post('/orders', _this.order)
             .then((response) => {
               toastr.success('Sucesso!','Pedido creado con sucesso!');
