bce9b484b9c592ff03d1b5f77d28d21026312f04
brunosmagna <bruno@magnaestrategia.com>
2016-10-30 00:09:20 -0200

    ajuste pedidos - comentário

--- app/Order.php
+++ app/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
     //use Notifiable;
 
     protected $fillable = ['cost','price','overalldiscount','status_id','customer_id','representative_id',
-        'representative_comision','representative_discount', 'delivery_id'];
+        'representative_comision','representative_discount', 'delivery_id', 'comment'];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 
     public function products(){
