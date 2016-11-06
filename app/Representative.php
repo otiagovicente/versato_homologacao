8597e94697b610a8d7ebce0cb43beac5c4ff2248
Tiago Vicente <tiago@magnaestrategia.com>
2016-11-06 16:48:42 -0300

    fix representative

---
+++ app/Representative.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
+
+class Representative extends Model
+{
+    use Searchable;
+
+    protected $fillable = ['code', 'user_id'];
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+    public function regions(){
+        return $this->belongsToMany('App\Region')->withTimestamps();
+    }
+
+    public function brands(){
+        return $this->belongsToMany('App\Brand')->withPivot('comission')->withTimestamps();
+    }
+
+    public function orders(){
+        return $this->belongsToMany(Order::class);
+    }
+
+}
