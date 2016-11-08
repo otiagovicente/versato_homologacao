1df349b6c8b3f4cefa93433cfd894c890d7bd2cc
Tiago Vicente <tiago@magnaestrategia.com>
2016-11-08 18:08:58 -0300

    Color Request

--- resources/views/colors/show.blade.php
+++ resources/views/colors/show.blade.php
@@ -25,7 +25,7 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-10">
             <list-products :id="{{$color->id}}" type="color"></list-products>
         </div>
     </div>
