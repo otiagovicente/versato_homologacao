72ad626b2e07716b31ce5e21c7e73a327919d087
brunosmagna <bruno@magnaestrategia.com>
2016-12-07 00:23:49 -0200

    problemas de infowindow

--- resources/assets/js/components/macroregions/MacroRegionsForm.vue
+++ resources/assets/js/components/macroregions/MacroRegionsForm.vue
@@ -8,59 +8,59 @@
         position: absolute;
         z-index:2;
     }
-    	.gm-style-iw {
-	width: 350px !important;
-	top: 15px !important;
-	left: 0px !important;
-	background-color: #fff;
-	box-shadow: 0 1px 6px rgba(178, 178, 178, 0.6);
-	border: 1px solid rgba(72, 181, 233, 0.6);
-	border-radius: 2px 2px 10px 10px;
-}
-.iw-container {
-	margin-bottom: 10px;
-	width: 100%;
-}
-.iw-container .iw-title {
-	font-family: 'Open Sans Condensed', sans-serif;
-	font-size: 22px;
-	font-weight: 400;
-	padding: 10px;
-	background-color: #48b5e9;
-	color: white;
-	margin: 0;
-	border-radius: 2px 2px 0 0;
-}
-.iw-container .iw-content {
-	font-size: 13px;
-	line-height: 18px;
-	font-weight: 400;
-	margin-right: 1px;
-	padding: 15px 5px 20px 15px;
-	max-height: 140px;
-	overflow-y: auto;
-	overflow-x: hidden;
-}
-.iw-content img {
-	float: right;
-	margin: 0 5px 5px 10px;	
-}
-.iw-subTitle {
-	font-size: 16px;
-	font-weight: 700;
-	padding: 5px 0;
-}
-.iw-bottom-gradient {
-	position: absolute;
-	width: 326px;
-	height: 25px;
-	bottom: 10px;
-	right: 18px;
-	background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
-	background: -webkit-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
-	background: -moz-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
-	background: -ms-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
-}
+    .gm-style-iw {
+        width: 350px !important;
+        top: 15px !important;
+        left: 0px !important;
+        background-color: #fff;
+        box-shadow: 0 1px 6px rgba(178, 178, 178, 0.6);
+        border: 1px solid rgba(72, 181, 233, 0.6);
+        border-radius: 2px 2px 10px 10px;
+    }
+    .iw-container {
+        margin-bottom: 10px;
+        width: 100%;
+    }
+    .iw-container .iw-title {
+        font-family: 'Open Sans Condensed', sans-serif;
+        font-size: 22px;
+        font-weight: 400;
+        padding: 10px;
+        background-color: #48b5e9;
+        color: white;
+        margin: 0;
+        border-radius: 2px 2px 0 0;
+    }
+    .iw-container .iw-content {
+        font-size: 13px;
+        line-height: 18px;
+        font-weight: 400;
+        margin-right: 1px;
+        padding: 15px 5px 20px 15px;
+        max-height: 140px;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
+    .iw-content img {
+        float: right;
+        margin: 0 5px 5px 10px;	
+    }
+    .iw-subTitle {
+        font-size: 16px;
+        font-weight: 700;
+        padding: 5px 0;
+    }
+    .iw-bottom-gradient {
+        position: absolute;
+        width: 326px;
+        height: 25px;
+        bottom: 10px;
+        right: 18px;
+        background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+        background: -webkit-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+        background: -moz-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+        background: -ms-linear-gradient(top, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
+    }
 </style>
 
 <template>
@@ -84,9 +84,8 @@
 
     <div class="map" v-el:macroregionmap style="width:100%;height:800px;"></div>
 
-    <div v-for="lstPolygons in polygon">
-        oi
-        <div v-bind:id="amem-$index" class="iw-container">
+    <div v-for="polygon in lstPolygons" class="infowindow-modal">
+        <div v-bind:id="lstPolygons[$index].inside_id" class="iw-container" >
             <div class="iw-title">Macro Região</div>
             <div class="iw-content">
                 <div class="row">
@@ -110,11 +109,9 @@
         </div>
     </div>
 
-
-
 </template>
 
-<script type="text/babel">
+<script>
     import toastr from 'toastr'
 	
     export default{
@@ -130,7 +127,7 @@
                 lstPolygons:[],
                 infowindow:null,
                 macroregions: {},
-                teste: 'alok',
+                inside_id:0,
             }
         },
 
@@ -167,7 +164,7 @@
                 };
 
                 _Macroregion.drawingManager = new google.maps.drawing.DrawingManager({
-                    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+                    drawingMode: null,//google.maps.drawing.OverlayType.POLYGON,
                     drawingControl: true,
                     drawingControlOptions: {
                         position: google.maps.ControlPosition.TOP_CENTER,
@@ -264,6 +261,7 @@
                    //console.log(polygon.getPath().getArray());
                 });
                 //_Macroregion.setSelection(polygon);
+                polygon.set('inside_id', _Macroregion.inside_id++);
                 _Macroregion.lstPolygons.push(polygon);
             },
             
@@ -273,28 +271,8 @@
                 _Macroregion.infowindow.open(_Macroregion.googleMap);
             },
             loadInfoWindowContent(polygon){
-                return '<div id="iw-container">'+
-                '    <div class="iw-title">Macro Região</div>'+
-                '    <div class="iw-content">'+
-                '        <div class="row">'+
-                '            <div class="col-md-10">'+
-                '                <span class="blue">Codigo</span>'+
-                '                <div class="form-group form-line-input" id="code">'+
-                '                    <input id="code-input" v-model="teste" class="form-control input-sm" type="text" value="'+polygon.code+'"/>'+
-                '                </div>'+
-                '            </div>'+
-                '        </div>'+
-                '        <div class="row">'+
-                '            <div class="col-md-10">'+
-                '                <span class="blue">Descrición</span>'+
-                '                <div class="form-group form-line-input" id="description">'+
-                '                    <input id="description-input" class="form-control input-sm" type="text" value="'+polygon.description+'"/>'+
-                '                </div>'+
-                '            </div>'+
-                '        </div>'+
-                '    </div>'+
-                '    <div class="iw-bottom-gradient"></div>'+
-                '</div>';
+                console.log($('.iw-container#'+ polygon.inside_id).html());
+                return $('.iw-container#'+ polygon.inside_id).html();
             },
             /*
              * Funções de manipulação do GoogleMaps
