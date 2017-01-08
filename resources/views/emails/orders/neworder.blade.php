51852779e5fa80fed03f7c01596c0c746e0310bc
brunosmagna <bruno@magnaestrategia.com>
2017-01-07 22:09:46 -0200

    ajuste de e-mail para inclusão de pivot

--- resources/views/emails/orders/neworder.blade.php
+++ resources/views/emails/orders/neworder.blade.php
@@ -107,7 +107,7 @@
                 </td>
                 <td valign="center" align="center" bgcolor="#ffffff" width="10%">
                     <span style="font-family: Helvetica, 'Arial', sans-serif; font-size:12px; font-weight:normal; color: #000; margin-top: 15px;   width: 100%; text-align:center">
-                        {{$product->representative_discount? $product->representative_discount : 0}}%
+                        {{$product->pivot->representative_discount? product->pivot->representative_discount : 0}}%
                     </span>
                 </td>
                 <td valign="center" align="right" bgcolor="#ffffff" width="16%">
